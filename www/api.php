d4fba6f8a296bbdd80841a62cf82afc1e94d68b5
jimmi <jimmirbn@gmail.com>
2015-05-04 23:06:12 +0200

    add lejringsdata.js

--- www/api.php
+++ www/api.php
@@ -8,5 +8,16 @@ if ($_POST['type'] == patientInfo) {
         $arr[] = $row;
     }
 	echo json_encode($arr);
-}	
+}
+
+if ($_POST['type'] == patientlejring) {
+	$result = $mysqli->query("SELECT patientimages.image, patientimages.imagedate, patientnotes.note, patientnotes.notedate, patientvideos.video, patientvideos.videodate FROM patientimages, patientnotes, patientvideos where patientimages.patientid = 1 and patientimages.type = 'lejring' and patientnotes.type = 'lejring' and patientvideos.type = 'lejring'");
+	$arr = array();
+	 while ($row = $result->fetch_assoc()) {
+        $arr[] = $row;
+    }
+	echo json_encode($arr);
+}
+
+
 ?>
--- www/js/site.js
+++ www/js/site.js
@@ -62,6 +62,23 @@ var mySwiper9 = myApp.swiper('.positioning-notes', {
     observeParents: true,
 }); 
 
+var type = "patientlejring";
+
+$.post("api.php", {"type": type}, function (data) {
+    var result = JSON.parse(data);
+    for (var i = 0; i < result.length; i++) {
+        var image = result[i].image;
+        var imagedate = result[i].imagedate;
+        var note = result[i].note;
+        var notedate = result[i].notedate;
+        var video = result[i].video;
+        var videodate = result[i].videodate;
+
+        $("#positioning-image").append('<img src="'+image+'">');
+        $("#positioning-video").append('<video><source type="video/mp4" src="'+video+'"></video>');
+
+    }
+});
 var type = "patientInfo";
 
 $.post("api.php", {"type": type}, function (data) {
