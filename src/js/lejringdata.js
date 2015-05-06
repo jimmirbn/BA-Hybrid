e95ce756c9d3ba53c1550d153bb56bf37348ab9b
jimmi <jimmirbn@gmail.com>
2015-05-06 22:27:19 +0200

    add if localstorage = null

--- src/js/lejringdata.js
+++ src/js/lejringdata.js
@@ -1,17 +1,1 @@
-var type = "patientlejring";
-var lastID = localStorage.getItem("lastPatient")
 
-$.post("http://www.digitaljimmi.com/api.php", {"type": type, 'id':lastID}, function (data) {
-    var result = JSON.parse(data);
-    for (var i = 0; i < result.length; i++) {
-        var image = result[i].image;
-        var imagedate = result[i].imagedate;
-        var note = result[i].note;
-        var notedate = result[i].notedate;
-        var video = result[i].video;
-        var videodate = result[i].videodate;
-
-        $("#positioning-image").append('<div class="swiper-slide test"><a href="#" class="openPhoto"><img class="photo" src="'+image+'"></a></div>');
-        $("#positioning-video").append('<a href="#" class="openVideo"><video><source type="video/mp4" src="'+video+'"></video></a>');
-    }
-});
--- src/js/patientInfo.js
+++ src/js/patientInfo.js
@@ -36,6 +36,24 @@ $(document).on("click", ".getPatientInfo", function() {
             patientText.text(infotext);
         }
     });
+
+    var type2 = "patientlejring";
+// var lastID = localStorage.getItem("lastPatient")
+
+$.post("http://www.digitaljimmi.com/api.php", {"type": type2, 'id':patientID}, function (data) {
+    var result = JSON.parse(data);
+    for (var i = 0; i < result.length; i++) {
+        var image = result[i].image;
+        var imagedate = result[i].imagedate;
+        var note = result[i].note;
+        var notedate = result[i].notedate;
+        var video = result[i].video;
+        var videodate = result[i].videodate;
+
+        $("#positioning-image").append('<div class="swiper-slide test"><a href="#" class="openPhoto"><img class="photo" src="'+image+'"></a></div>');
+        $("#positioning-video").append('<a href="#" class="openVideo"><video><source type="video/mp4" src="'+video+'"></video></a>');
+    }
+});
 });
 
 //Load last patient
